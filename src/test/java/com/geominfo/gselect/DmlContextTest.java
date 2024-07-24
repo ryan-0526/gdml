fe282b2901886f371e0e9657eb7a6b4f0f8b4201
xqh <1187773467@qq.com>
2024-07-24 16:35:11 +0800

    111

--- src/test/java/com/geominfo/gselect/DmlContextTest.java
+++ src/test/java/com/geominfo/gselect/DmlContextTest.java
@@ -7,6 +7,7 @@ public class DmlContextTest {
 
     public static void main(String[] args) {
         ThinQuery thinQuery = JsonTest.getThinQuery();
+
         //数据库类型在 ThinQuery对象根据 cubeId 查询模型后能获取到，所以这里不用显示制定了，不然使用者也不知道 (废弃)
 //        DmlContext dmlContext = new DmlContext(DialectType.ORACLE);
         String selectSql = new DmlContext()
